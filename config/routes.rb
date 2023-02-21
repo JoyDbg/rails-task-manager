24b0228eb4f722e72b4591fa17718b5948ac3e6f
Joy Danenberg <joy.danenberg@gmail.com>
2023-02-21 14:57:32 +0100

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,8 @@ Rails.application.routes.draw do
   get "tasks", to: "tasks#index"
   get "tasks/:id", to: "tasks#show", as: :task
   get "task/new", to: "tasks#new", as: :new_task
-  post "tasks/new", to: "tasks#create"
+  post "tasks", to: "tasks#create"
   get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
   patch "tasks/:id", to: "tasks#update", as: :update_task
+  delete "tasks/:id", to: "tasks#destroy", as: :destroy_task
 end
