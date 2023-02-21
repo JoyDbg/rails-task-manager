dc3ed743cbcdce519fff2c7776b3206fc0cbace2
Joy Danenberg <joy.danenberg@gmail.com>
2023-02-21 13:03:04 +0100

    exo2

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "tasks", to: "tasks#index"
+  get "tasks/:id", to: "tasks#show", as: :task
+  get "task/new", to: "tasks#new", as: :new_task
+  post "tasks/new", to: "tasks#create"
+  get "tasks/:id/edit", to: "tasks#edit", as: :edit_task
+  patch "tasks/:id", to: "tasks#update", as: :update_task
 end
